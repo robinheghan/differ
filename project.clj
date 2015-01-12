30f12110676582da94c7264a395fd119808e7de0
Robin Heggelund Hansen <skinneyz89@gmail.com>
2015-01-13 00:41:57 +0100

    Don't include cljx as a runtime dependency

--- project.clj
+++ project.clj
@@ -6,33 +6,35 @@
 
   :dependencies [[org.clojure/clojure "1.6.0"]]
 
-  :profiles {:dev {:plugins [[com.keminglabs/cljx "0.4.0"]]
-                   :jar-exclusions [#"\.cljx"]
-                   :hooks [cljx.hooks]
-                   :cljx {:builds [{:source-paths ["src"]
-                                    :output-path "target/classes"
-                                    :rules :clj}
-                                   {:source-paths ["src"]
-                                    :output-path "target/classes"
-                                    :rules :cljs}
-                                   {:source-paths ["test"]
-                                    :output-path "target/test-classes"
-                                    :rules :clj}
-                                   {:source-paths ["test"]
-                                    :output-path "target/test-classes"
-                                    :rules :cljs}]}}
+  :source-paths ["src" "target/classes"]
+  :test-paths ["target/test-classes"]
+  :jar-exclusions [#"\.cljx"]
+  :prep-tasks [["cljx-once"]]
+
+  :profiles {:cljx {:plugins [[com.keminglabs/cljx "0.4.0"]]
+                    :cljx {:builds [{:source-paths ["src"]
+                                     :output-path "target/classes"
+                                     :rules :clj}
+                                    {:source-paths ["src"]
+                                     :output-path "target/classes"
+                                     :rules :cljs}
+                                    {:source-paths ["test"]
+                                     :output-path "target/test-classes"
+                                     :rules :clj}
+                                    {:source-paths ["test"]
+                                     :output-path "target/test-classes"
+                                     :rules :cljs}]}}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}
-             :cljs {:dependencies [[org.clojure/clojurescript "0.0-2371"]]
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha5"]]}
+             :cljs {:dependencies [[org.clojure/clojurescript "0.0-2665"]]
                     :plugins [[lein-cljsbuild "1.0.3"]
-                              [com.cemerick/clojurescript.test "0.3.1"]]
+                              [com.cemerick/clojurescript.test "0.3.3"]]
                     :cljsbuild {:test-commands {"phantom" ["phantomjs" :runner "target/testable.js"]}
                                 :builds [{:source-paths ["target/classes" "target/test-classes"]
                                          :compiler {:output-to "target/testable.js"
                                                     :optimizations :whitespace}}]}
-                    :hooks [cljx.hooks
-                            leiningen.cljsbuild]}}
+                    :prep-tasks [["cljsbuild" "once"]]
+                    :hooks [leiningen.cljsbuild]}}
 
-  :source-paths ["src" "target/classes"]
-  :test-paths ["target/test-classes"]
-  :aliases {"all-tests" ["with-profile" "cljs:1.6:1.7" "test"]})
+  :aliases {"all-tests" ["with-profile" "cljs:1.6:1.7" "test"]
+            "cljx-once" ["with-profile" "cljx" "cljx" "once"]})
