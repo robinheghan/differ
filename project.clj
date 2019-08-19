31142bbc19aa0f5e90424bf54525d0232ff8bf99
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2019-08-19 19:53:13 +0200

    Updated clojurescript version, add 1.9 and 1.10 targets. Remove 1.7 support.

--- project.clj
+++ project.clj
@@ -4,11 +4,12 @@
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
 
-  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]]
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]]
 
-  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :cljs {:dependencies [[org.clojure/clojurescript "1.7.228"]]
+  :profiles {:1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.1"]]}
+             :cljs {:dependencies [[org.clojure/clojurescript "1.10.520"]]
                     :plugins [[lein-cljsbuild "1.1.2"]]
                     :cljsbuild {:test-commands {"phantom" ["phantomjs" :runner "target/testable.js"]}
                                 :builds [{:source-paths ["src" "test"]
@@ -16,4 +17,4 @@
                                                      :optimizations :none}}]}
                     :prep-tasks [["cljsbuild" "once"]]}}
 
-  :aliases {"all-tests" ["with-profile" "cljs:1.7:1.8" "test"]})
+  :aliases {"all-tests" ["with-profile" "cljs:1.8:1.9:1.10" "test"]})
